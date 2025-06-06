a312d9cb6472cd944a6c317c8bd22e998fb8151b
Quyen <quyenhgxy@gmail.com>
2025-06-06 22:27:42 +0700

    tt

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 // import mongoose from "mongoose"
 require("dotenv").config();
-
+//DB_URL=mongodb+srv://quyenhgaz:123456az@cluster0.4zbrgd8.mongodb.net/photoapp?retryWrites=true&w=majority&appName=Cluster0
+// JWT_SECRET=final_project
+// NODE_ENV=development
 async function dbConnect() {
   mongoose
     .connect(process.env.DB_URL)
