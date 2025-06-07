d185830bbd64b5baf00a5f2f95f9b40c7d09ef1a
Quyen <quyenhgxy@gmail.com>
2025-06-07 14:11:19 +0700

    tt

--- backend/lib/generateToken.js
+++ backend/lib/generateToken.js
@@ -6,6 +6,9 @@ const generateToken = (userId, res) => {
         process.env.JWT_SECRET,
         { expiresIn: '7d' }
     );
+    // httpOnly: true,
+    //     secure: true,
+    //     sameSite: "None",
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
